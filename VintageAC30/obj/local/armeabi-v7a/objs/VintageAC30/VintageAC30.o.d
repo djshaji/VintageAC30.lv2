5608860b1e8e8bbbad3139d5578e6fbb01b3057d
Shaji Khan <git@shaji.in>
2023-04-21 13:13:09 +0530

    ndk build

---
+++ VintageAC30/obj/local/armeabi-v7a/objs/VintageAC30/VintageAC30.o.d
@@ -0,0 +1,24 @@
+./obj/local/armeabi-v7a/objs/VintageAC30/VintageAC30.o: \
+  jni/VintageAC30.cpp jni/voxac30normal.cc \
+  jni/voxac30_normal_neg_table.h jni/voxac30_normal_table.h \
+  jni/voxac30topboost.cc jni/voxac30_stage1_neg_table.h \
+  jni/voxac30_stage1_table.h jni/voxac30poweramp.cc \
+  jni/voxac30_poweramp_neg_table.h jni/voxac30_poweramp_table.h
+
+jni/voxac30normal.cc:
+
+jni/voxac30_normal_neg_table.h:
+
+jni/voxac30_normal_table.h:
+
+jni/voxac30topboost.cc:
+
+jni/voxac30_stage1_neg_table.h:
+
+jni/voxac30_stage1_table.h:
+
+jni/voxac30poweramp.cc:
+
+jni/voxac30_poweramp_neg_table.h:
+
+jni/voxac30_poweramp_table.h:
